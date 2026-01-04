85c2335916834af8130ff4b9440b74927d15abdd
LAPTOP-MFIC9KH1\ACER <ann263779@gmail.com>
2026-01-05 01:27:57 +0700

    Add transfer history BE

---
+++ src/main/java/com/vti/springdatajpa/dto/TransferHistoryDTO.java
@@ -0,0 +1,18 @@
+package com.vti.springdatajpa.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class TransferHistoryDTO {
+    private Integer id;
+    private String partnerName;
+    private String direction;  // IN, OUT
+    private Double amount;
+    private String status;
+    private String note;
+    private LocalDateTime createdAt;
+    private String type;
+    private String referenceId;
+}
+
